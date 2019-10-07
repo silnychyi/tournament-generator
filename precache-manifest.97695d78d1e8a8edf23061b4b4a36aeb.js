c54950aec4d0e6f455ad477f8dfea14ca0e08edc
silnychyi <silnichii@gmail.com>
2019-10-07 18:03:14 +0200

    Updates

---
+++ precache-manifest.97695d78d1e8a8edf23061b4b4a36aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8bcd80b4ea29f0a235dcae7a3f1145",
+    "url": "/tournament-generator/index.html"
+  },
+  {
+    "revision": "817e126cf06932314db5",
+    "url": "/tournament-generator/static/css/main.c28292a0.chunk.css"
+  },
+  {
+    "revision": "f4c0eefba5a3a004a1a5",
+    "url": "/tournament-generator/static/js/2.2056c782.chunk.js"
+  },
+  {
+    "revision": "817e126cf06932314db5",
+    "url": "/tournament-generator/static/js/main.f9a3c5a9.chunk.js"
+  },
+  {
+    "revision": "b218299c7defe7fceec3",
+    "url": "/tournament-generator/static/js/runtime-main.6c5395b1.js"
+  },
+  {
+    "revision": "9eb38563234bc7391d05bd5131a636b0",
+    "url": "/tournament-generator/static/media/preloader.9eb38563.gif"
+  }
+]);
